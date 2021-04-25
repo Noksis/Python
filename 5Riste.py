da5bb8a1bf59757be7ad0283572be8845f2500e5
Nikita Valov <55804292+Noksis@users.noreply.github.com>
2021-04-25 16:29:17 +0300

    Update 5Riste.py

--- 5Riste.py
+++ 5Riste.py
@@ -17,3 +17,26 @@ for i in Data["SEX"]:
         Mans += 1
 print("Mans procent is", Mans/len(Data)*100,"%")
 print("Womens procent is", (1 - Mans/len(Data)) * 100, "%")
+
+fig = plt.figure()
+plt.hist(Data["AGE"])
+plt.title('Hist of age')
+plt.xlabel("Age")
+plt.ylabel("Numbers of people")
+plt.grid(True)
+
+defaults = Data["default.payment.next.month"].sum()
+No_defaults = len(Data) - defaults
+print(defaults)
+print(No_defaults)
+
+
+fig = plt.figure()
+plt.bar(["Defaulst","Nodefaults"], [defaults,No_defaults])
+plt.title('Hist of defaults')
+plt.xlabel("Status")
+plt.ylabel("Numbers of people")
+plt.grid(True)
+
+plt.show()
+#plt.bat()
